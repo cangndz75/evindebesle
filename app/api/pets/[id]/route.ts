b6521d18c858b03a758c8c73797664b45561e203
Can Gündüz <cangunduz0001@gmail.com>
2025-07-12 04:56:54 +0300

    1.0.3.8

--- app/api/pets/[id]/route.ts
+++ app/api/pets/[id]/route.ts
@@ -1,36 +1,39 @@
-// import { NextRequest, NextResponse } from "next/server"
-// import { prisma } from "@/lib/db"
+import { NextRequest, NextResponse } from "next/server"
+import { prisma } from "@/lib/db"
 
-// export async function PUT(req: NextRequest) {
-//   const url = new URL(req.url)
-//   const id = url.pathname.split("/").pop() || "" 
+export async function PUT(req: NextRequest) {
+  const url = new URL(req.url)
+  const id = url.pathname.split("/").pop() || "" 
 
-//   const body = await req.json()
+  const body = await req.json()
 
-//   try {
-//     const updatedPet = await prisma.pet.update({
-//       where: { id },
-//       data: body,
-//     })
+  try {
+    const updatedPet = await prisma.pet.update({
+      where: { id },
+      data: body,
+    })
 
-//     return NextResponse.json(updatedPet)
-//   } catch (error) {
-//     return NextResponse.json({ error: "Güncelleme hatası" }, { status: 500 })
-//   }
-// }
+    return NextResponse.json(updatedPet)
+  } catch (error) {
+    return NextResponse.json({ error: "Güncelleme hatası" }, { status: 500 })
+  }
+}
 
 
-// export async function DELETE(req: NextRequest) {
-//   const url = new URL(req.url)
-//   const id = url.pathname.split("/").pop() || ""
+export async function DELETE(req: NextRequest) {
+  const url = new URL(req.url)
+  const id = url.pathname.split("/").pop() || ""
 
-//   try {
-//     await prisma.pet.delete({
-//       where: { id },
-//     })
+  try {
+    await prisma.pet.delete({
+      where: { id },
+    })
 
-//     return NextResponse.json({ success: true })
-//   } catch (error) {
-//     return NextResponse.json({ success: false, error: "Silme işlemi başarısız" }, { status: 500 })
-//   }
-// }
+    return NextResponse.json({ success: true })
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: "Silme işlemi başarısız" },
+      { status: 500 }
+    )
+  }
+}

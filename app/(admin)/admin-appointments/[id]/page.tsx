772fb8d933630f5273955478cbf7df9f919c2a17
Can Gündüz <cangunduz0001@gmail.com>
2025-07-16 19:52:20 +0300

    1.1.9.6 | appointment | admin

--- app/(admin)/admin-appointments/[id]/page.tsx
+++ app/(admin)/admin-appointments/[id]/page.tsx
@@ -1,14 +1,16 @@
 import AppointmentDetailCard from "../_components/AppoinmentDetailCard";
 
-export default function AppointmentDetailPage({
+export default async function AppointmentDetailPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>; 
 }) {
+  const { id } = await params;
+
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-6">Randevu Detayı</h1>
-      <AppointmentDetailCard appointmentId={params.id} />
+      <AppointmentDetailCard appointmentId={id} />
     </div>
   );
 }

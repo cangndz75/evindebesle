9ef1b2458abb608f931e5e27f183c21522ae7b58
Can Gündüz <cangunduz0001@gmail.com>
2025-08-10 21:51:02 +0300

    1.5.2.3

--- app/(public)/home/page.tsx
+++ app/(public)/home/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryMarquee from "../_components/CategoryMarquee";
 import CategorySection from "../_components/CategorySection";
 import Footer from "../_components/Footer";
@@ -8,13 +9,25 @@ import HowItWorksSection from "../_components/HowItWorksSection";
 import PetTypeSelector from "../_components/PetTypeSelector";
 import SearchBox from "../_components/SearchBox";
 
-
 export default function HomePage() {
   return (
     <>
       <Hero />
       {/* <SearchBox /> */}
       <HowItWorksSection />
+      <section className="my-6 sm:my-10">
+        <div className="w-full">
+          <Image
+            src="https://res.cloudinary.com/dlahfchej/image/upload/v1754850604/banner2_upt5zl.png"
+            alt="Randevu banner"
+            width={7924}
+            height={1139}
+            className="w-full h-auto object-contain rounded-xl shadow-lg"
+            sizes="100vw"
+            priority
+          />
+        </div>
+      </section>
       <PetTypeSelector />
       <CategoryMarquee />
       <FaqSection />
